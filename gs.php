ec7c5bd2653726602bd859b16d5eb37c6c35454b
Avi Ginsberg <avi.ginsberg@gmail.com>
2017-02-03 22:13:48 -0500

    Updates

--- gs.php
+++ gs.php
@@ -14,21 +14,28 @@
  * The goodness of a match for an individual can be measured by the position in the preference list of the match,w. The overall goodness for the M's would be the sum over each m , of his rank for the matching w. Similarly, the goodness for the W 's can be defined.
  *
  */
+
+ini_set('memory_limit','3G');
+
 require_once "man.php";
 require_once "woman.php";
 
 //Number of pairs to generate
-$number_of_pairs = 20;
+$number_of_pairs = 100;
+
+//Should we output the result to a CSV file?
+//Note: csv file does not have column headers. they must be added manually.
+//The columns are: number of pairs, man match goodness, woman match goodness
+$output_to_csv = true;
 
 
 
 $mated_men = Array();
 
 //initialize our men and women objects
-for ($i = 1; $i <= $number_of_pairs; $i++)
-{
-    $free_men['M'.$i] = new man($number_of_pairs,'M'.$i);
-    $women['W'.$i] = new woman($number_of_pairs,'W'.$i);
+for ($i = 1; $i <= $number_of_pairs; $i++) {
+    $free_men['M' . $i] = new man($number_of_pairs, 'M' . $i);
+    $women['W' . $i] = new woman($number_of_pairs, 'W' . $i);
 }
 
 
@@ -42,12 +49,15 @@ function check_for_remaining_free_men()
     global $number_of_pairs, $free_men;
 
     //check if we have men left in the free_men array. if not, return false.
-    if (count($free_men)<=0){return false;}
+    if (count($free_men) <= 0) {
+        return false;
+    }
 
     //check each man left in the free_man array to see if they have proposed to every woman
-    foreach ($free_men as $freeman)
-    {
-        if(count($freeman->get_unique_proposal_list()) < $number_of_pairs){return true;}
+    foreach ($free_men as $freeman) {
+        if (count($freeman->get_unique_proposal_list()) < $number_of_pairs) {
+            return true;
+        }
     }
 
     //if we get here we know that there are no free men remaining so return false
@@ -56,19 +66,22 @@ function check_for_remaining_free_men()
 
 function logmsg($msg)
 {
-    echo "\n".$msg;
+    echo "\n" . $msg;
 }
 
-while (check_for_remaining_free_men())
-{
-    foreach ($free_men as $m)
-    {
+$loops = 0;
+
+
+while (check_for_remaining_free_men()) {
+    $loops++;
+    echo "\n\n\nLoop $loops\nCurrent memory usage: " . (round(memory_get_usage() / 1024)) . "mb";
+
+    foreach ($free_men as $m) {
         $w = $m->get_best_woman_not_proposed_to();
         $manID = $m->get_name();
 
         //if w is free (m,w) become engaged
-        if(!$women[$w]->check_if_currently_free())
-        {
+        if (!$women[$w]->check_if_currently_free()) {
 
 
             //log the proposal
@@ -88,24 +101,19 @@ while (check_for_remaining_free_men())
             //set the woman's mate to the man & update her status
             $women[$w]->set_mate($manID);
             $women[$w]->set_mated_status(true);
-        }
-        //else if w is currently engaged to another man
-        else
-        {
+        } //else if w is currently engaged to another man
+        else {
             //if w prefers m' to m then m remains free
             //logmsg("checking if ".$women[$w]->get_mate()." is more desirable");
             //print_r($women[$w]->get_pref_list());
-            if($women[$w]->get_pref_list_pos_of_man($women[$w]->get_mate()) < $women[$w]->get_pref_list_pos_of_man($manID))
-            {
+            if ($women[$w]->get_pref_list_pos_of_man($women[$w]->get_mate()) < $women[$w]->get_pref_list_pos_of_man($manID)) {
                 //log the proposal
                 $free_men[$manID]->log_proposal($w);
 
                 //say what's going on
                 logmsg("$manID proposed to $w. $w was already mated and preferred her current mate to $manID. $manID remains free.");
-            }
-            //else w prefers m to m': (m,w) become engaged and m' becomes free
-            else
-            {
+            } //else w prefers m to m': (m,w) become engaged and m' becomes free
+            else {
                 //log the proposal
                 $free_men[$manID]->log_proposal($w);
 
@@ -136,8 +144,6 @@ while (check_for_remaining_free_men())
         }
 
 
-
-
     }
 
 
@@ -148,27 +154,25 @@ $m_goodness_sum = 0;
 $w_goodness_sum = 0;
 
 echo "\n\n\n\n\nPrinting final pairs:\n";
-foreach($mated_men as $m)
-{
+foreach ($mated_men as $m) {
     $m_name = $m->get_name();
     $w_name = $m->get_mate();
     $m_match_goodness = $m->get_match_goodness();
     $m_goodness_sum += $m_match_goodness;
     $w_match_goodness = $women[$w_name]->get_match_goodness();
     $w_goodness_sum += $w_match_goodness;
-    echo "\n$m_name is engaged to ".$m->get_mate()." | man match goodness: $m_match_goodness | woman match goodness: $w_match_goodness";
+    echo "\n$m_name is engaged to " . $m->get_mate() . " | man match goodness: $m_match_goodness | woman match goodness: $w_match_goodness";
 }
 
-echo "\n\nAverage man match goodness: ".(round(($m_goodness_sum/$number_of_pairs),3)).
-        "\nAverage woman match goodness: ".(round(($w_goodness_sum/$number_of_pairs),3));
+echo "\n\nAverage man match goodness: " . (round(($m_goodness_sum / $number_of_pairs), 3)) .
+    "\nAverage woman match goodness: " . (round(($w_goodness_sum / $number_of_pairs), 3));
 
 
-
-
-
-
-
-
+if ($output_to_csv) {
+    $output = '"' . $number_of_pairs . '",' . '"' . (round(($m_goodness_sum / $number_of_pairs), 3)) . '",' . '"' . (round(($w_goodness_sum / $number_of_pairs), 3)) . '"';
+    if (file_put_contents('gs-output.csv', $output . PHP_EOL, FILE_APPEND | LOCK_EX))
+        echo "\n\nResults appended to gs-output.csv successfully.";
+}
 
 
 
